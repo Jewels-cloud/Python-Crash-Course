9c09a7e1b176cdf0341b9305eb107fbc98ec97f5
davidsonjewels <davidsonjewels62@gmail.com>
2024-07-21 19:00:25 +0100

    initial commit

---
+++ Chapter_two/ex2-1.py
@@ -0,0 +1,8 @@
+# Simple Message: Assign a message to a variable, and then print that message
+# Assign a message to a variable:
+message = "hello Cadence"
+
+# Print that message
+print (message) 
+
+
---
+++ Chapter_two/ex2-2.py
@@ -0,0 +1,11 @@
+# Assign a message to a variable:
+message = "Hello Cadence"
+
+# Print that message
+print (message)
+
+# Then change the value of the variable to a new message
+message = "Now, Im Jewels"
+
+# Print the new message
+print (message)
